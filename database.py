8fbbd82631a5cd8b85f9536970a39f0ce27501c8
as9891942570-dotcom <as9891942570@gmail.com>
2026-06-20 11:11:39 +0530

    modified database.py and schemas.py

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
-import pymysql
+import os
+import psycopg2
 
 def get_db_connection():
-    return pymysql.connect(
-        host="pg-2eeb428e-as9891942570-b4ae.i.aivencloud.com",
-        port=21960,
-        user="avnadmin",
-        password="AVNS_ncZYEMIRSIeUob-rnZU",
-        database="labour_app",
-        ssl={"ssl": {}},
-        cursorclass=pymysql.cursors.DictCursor
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        sslmode="require"
     )
