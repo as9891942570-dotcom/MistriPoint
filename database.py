d8fb08bb7fb92fa33dd90119f35819f173e0403f
as9891942570-dotcom <as9891942570@gmail.com>
2026-06-21 13:10:12 +0530

    data base changes

--- database.py
+++ database.py
@@ -1,10 +1,28 @@
-import psycopg
-from psycopg.rows import dict_row
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from urllib.parse import quote_plus
+from sqlalchemy import create_engine
+
 
 DATABASE_URL = "postgresql://neondb_owner:npg_05pqgFycUnjL@ep-summer-brook-atq95usp-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
-def get_db_connection():
-    return psycopg.connect(
-        DATABASE_URL,
-        row_factory=dict_row
-    )
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
