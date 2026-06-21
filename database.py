1e56af61f0aad032713bab80881e2bd682b0b193
as9891942570-dotcom <as9891942570@gmail.com>
2026-06-21 12:41:11 +0530

    database password

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import psycopg
 from psycopg.rows import dict_row
 
-DATABASE_URL = "postgresql://neondb_owner:YOUR_PASSWORD@ep-summer-brook-atq95usp-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require"
+DATABASE_URL = "postgresql://neondb_owner:npg_05pqgFycUnjL@ep-summer-brook-atq95usp-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 def get_db_connection():
     return psycopg.connect(
